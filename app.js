e268f7d8b6adb2e975ccb636cd364f567dcd1e8c
Diilaye <issa.k@verumko.com>
2023-04-10 20:25:48 +0000

    init git

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const port = 4200;
+
+app.use(express.static(__dirname + '/dist/zakat'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve('dist/zakat/index.html')));
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
